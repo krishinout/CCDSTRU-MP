5d68ada2d19e43571bddb9f596d3fb6c5c2f1377
HoH-cwbf-golden <0pinyowo@gmail.com>
2026-03-24 20:44:29 +0800

    helper.c

--- helper.c
+++ helper.c
@@ -37,6 +37,7 @@ void emptyBoard(gameSets *game)
             game->B[i][j] = 0;
             game->S[i][j] = 0;
             game->T[i][j] = 0;
+            game->I[i][j] = 0;
         }
     }
     game->amtR = 0;
@@ -113,7 +114,7 @@ void printBoard(gameSets *game)
 * @param go is an integer that represents a player's turn, 1 for R and 0 for B
 * @param row is an integer that represents the row position
 * @param col is an integer that represents the column position*/
-void Remove(gameSets *position, int go, position pos)
+void Remove(gameSets *position, int go, location pos)
 {
     if(go)
     {
@@ -134,25 +135,37 @@ void Remove(gameSets *position, int go, position pos)
 /* This function simulates a chain reaaction or explosion
  * @param sets is a pointer that represents the sets where specific cells will be stored
  * @param go determines a player's turn
- * @param pos is the position of the triggered cell*/
-void Expand(gameSets *sets, int go, position pos)
+ * @param pos is the location of the triggered cell*/
+void Expand(gameSets *sets, int go, location pos) //note: still has bugs?? when i was testing
 {
-    position u, d, k, r;
-    u.y = pos.row - 1;
-    d.y = pos.row + 1;
-    k.x = pos.col - 1;
-    r.x = pos.col - 1;
+    location u, d, k, r;
+    //--
+    u.row = pos.row - 1;
+    u.col = pos.col; //nakalimutan initialize yun mga col/row (prev garbage value lang sila)
+    //--
+    d.row = pos.row + 1;
+    d.col = pos.col;
+    //--
+    k.row = pos.row;
+    k.col = pos.col - 1;
+    //--
+    r.row = pos.row;
+    r.col = pos.col + 1;
 
     remove(sets, go, pos); // removes the cell that triggered the expansion
 
     // this portion replaces the the selected neighboring cells + a chance of a chain reaction
-    if(go)
-        replace(sets, go, u);
-    else
-        replace(sets, go, d);
-
-    replace(sets, go, k);
-    replace(sets, go, r);
+    if(go==1)
+        if (u.row >= 0)
+            Replace(sets, go, u);
+    else if(go==0)
+        if (d.row < 3)
+            Replace(sets, go, d);
+    
+    if (k.col >= 0)
+        Replace(sets, go, k);
+    if (r.col < 3)
+        Replace(sets, go, r);
 }
 
 /*This function checks the neighboring cells after expansion and determines whether a 
@@ -161,7 +174,7 @@ void Expand(gameSets *sets, int go, position pos)
  * @param go is a flag that determines a player's turn
  * @param pos is a struct that represents a specific cell's coordinates
   */
-void Replace(gameSets *sets, int go, position pos)
+void Replace(gameSets *sets, int go, location pos)
 {
     int found = 0; // like a flag that determines if may second explosion or expansion
 
@@ -231,7 +244,7 @@ void Replace(gameSets *sets, int go, position pos)
  * @Param good is an integer that acts as a flag that determines if if a cell was already chosen once and stored in set S
  * @param go is the flag that determines a player's turn 
  */
-void Update(gameSets *sets, position pos, int *good, int go)
+void Update(gameSets *sets, location pos, int *good, int go)
 {
     *good = 0; // resets it
 
@@ -257,63 +270,99 @@ void Update(gameSets *sets, position pos, int *good, int go)
     * @param start is a pointer that indicates if both players had done their first turn
     * @param row is an integer that represent the row position
     * @param col is an integer that represent the column position*/
-void nextPlayerMove(gameSets *position, int *go, int *good, int *over, int *val, int *start, int row, int col)
+void nextPlayerMove(gameSets *position, int *go, int *good, int *over, int *val, int *start)
 {
+    int valid = 0;
+    int x;
+    int y;
+    location input;
+
+
     if (*over == 0 && *start == 1 && *go == 1) // Player R's first turn
+    {
+        printf("Player R it's your move\n\n");
+        while(!valid)
         {
-            position->R[row][col] = 1;
-            position->amtR++;
-            position->S[row][col] = 1;
-            *good = 1;
+            inputCords(&x,&y);
+            if (position->I[x][y] == 0)
+            {
+                position->I[x][y] = 1;
+                position->R[x][y] = 1;
+                position->amtR++;
+                position->S[x][y] = 1;
+                *good = 1;
+                valid = 1;  
+            }
+            else
+                printf("\nPosistion is already taken\n\n");
         }
+    }
     else if (*over == 0 && *start == 1 && *go == 0) // Player B's first turn
+    {
+        printf("Player B it's your move\n\n");
+        while(!valid)
         {
-            position->B[row][col] = 1;
-            position->amtB++;
-            position->S[row][col] = 1;
+
+
+            inputCords(&x,&y);
+            if (position->I[x][y] == 0)
+            {
+                position->I[x][y] = 1;
+                position->B[x][y] = 1;
+                position->amtB++;
+                position->S[x][y] = 1;
+                *good = 1;
+                valid = 1;
+            }
+            else
+                printf("\nPosistion is already taken\n\n");
+        }
+    }
+    //------------------------------------------------------------------------//
+    if (*over == 0 && *start == 0 && *go == 1 )
+    {
+        printf("Player R it's your move\n\n");
+            while(!valid)
+            {
+                inputCords(&x,&y);
+                if (position->R[x][y] == 1)
+                    valid = 1;
+                else
+                    printf("\nPosistion invalid\n\n");
+            }
+            input.row = x;
+            input.col = y;
+            Update(position,input,good,*go);
+            printf("Hey you chose your taken spot Player R\n");
             *good = 1;
-        }
-    /*-------
-
-    if (*over == 0 *start == 0)
-        if(go) // Player R's turn
+    }
+    else if (*over == 0 && *start == 0 && *go == 0 )
+    {
+        printf("Player B it's your move\n\n");
+        while(!valid)
         {
-            if(position->R[row][col] == 1)
-            {
-                //Update();
-                *good = 1;
-            }
-            else if ((position->R[row][col] != 1))
-            {
-                // ok this where it gets complicated
-                // ask agained because pos !E R (idk how to do this yet) until its an elem of R
-            }
+            inputCords(&x,&y);
+            if (position->B[x][y] == 1)
+                valid = 1;
+            else
+                printf("\nPosistion invalid\n\n");
         }
-        else if (!go) // Player B's turn
-        {
-            if(position->B[row][col] == 1)
-            {
-                //Update();
-                *good = 1;
-            }
-            else if ((position->B[row][col] != 1))
-            {
-                // ok this where it gets complicated
-                // ask agained because pos !E R (idk how to do this yet) until its an elem of B
-            }
-        }
-
-
-    //(¬over ? ¬start ? (go ? pos ? R ? ¬go ? pos ? B)) ? (Update(pos) ? good = true)
-    -------*/
+        input.row = x;
+        input.col = y;
+        Update(position,input,good,*go);
+        printf("Hey you chose your taken spot Player B\n");
+        *good = 1;
+    }
+    //------------------------------------------------------------------------//
     if (*start == 1 && position->amtR == 1 && position->amtB == 1) // if both player already took their first turn
             *start = 0;
-    
+    //------------------------------------------------------------------------//
     if (*over == 0 && *good == 1) //if the player is done with their move
         {
             *good = !(*good);
             *go = !(*go);
             (*val)++;
         }
+
 
 }
