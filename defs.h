2ad9df0a97d93f660a5de89cc0f4590a58512989
Eggboi <fabialaiggy@gmail.com>
2026-03-26 19:50:27 +0800

    Added function prototypes
    Major updates:
    - Added all function prototypes to the main definition header file to avoid implicit declarations

--- defs.h
+++ defs.h
@@ -20,11 +20,16 @@ typedef struct
     int col;
 }location;
 
-
-void Expand(gameSets *sets, int go, location pos);
-void Replace(gameSets *sets, int go, location pos);
-// necessary for recurssion
-
+// Function Prototypes
+void replace(gameSets *sets, int go, location pos);
+void expand(gameSets *sets, int go, location pos);
+void inputCoords(int *x, int *y);
+void emptyBoard(gameSets *game);
+void printBoard(gameSets *game);
+void clearSet(gameSets *position, int playerTurn, location pos);
+void expand(gameSets *sets, int playerTurn, location pos);
+void update(gameSets *sets, location pos, int *check_charge, int playerTurn);
+void nextPlayerMove(gameSets *position, int *playerTurn, int *turn_end, int *game_end, int *rounds_elapsed, int *start);
 
 
 
