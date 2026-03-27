32f21572b40125fb7d2f31cf86ae6aab32083f6d
HoH-cwbf-golden <0pinyowo@gmail.com>
2026-03-27 20:48:52 +0800

    added the loop and gameOver phase

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
- 
 #include <stdio.h>
 #include <string.h>
 #include "helper.c"
+
+
 
 
 int main()
@@ -11,34 +12,34 @@ int main()
     int game_end = 0;
     int rounds_elapsed = 0;
     int start = 1;
-    int x;
-    int y;
+    int freeSpaces;
     gameSets position;
-    
+   
     emptyBoard(&position);
 
-	printf("Current Board\n\n");
-    printBoard(&position);
 
-    inputCoords(&x,&y);
+    //loop
+    while (!game_end)
+    {
+        freeSpaces = 9;
+        freeSpaces = freeSpaces - (position.amtR + position.amtB);
 
-    nextPlayerMove(&position, &playerTurn, &turn_end, &game_end, &rounds_elapsed, &start);
-    
-    printf("Current Board\n\n");
-	printBoard(&position);
-	
-	inputCoords(&x,&y);
+        printf("Current Board\n\n");
+        printBoard(&position);
 
-    nextPlayerMove(&position, &playerTurn, &turn_end, &game_end, &rounds_elapsed, &start);
-    
-    printf("Current Board\n\n");
-	printBoard(&position);
-	
-	printf("\n\n");
-	
-	printf("playerTurn : %d | turn_end : %d | game_end : %d | rounds_elapsed : %d | start : %d", playerTurn, turn_end, game_end, rounds_elapsed, start);
-    
-//currently after the start section palang
+        printf("\nplayerTurn : %d | turn_end : %d | game_end : %d \n", playerTurn, turn_end, game_end); //note: this for testing and debuggin
+        printf("rounds_elapsed : %d | start : %d | freeSpaces : %d \n\n", rounds_elapsed, start, freeSpaces); // we can edit this to improve the U.I
+
+        nextPlayerMove(&position, &playerTurn, &turn_end, &game_end, &rounds_elapsed, &start);
+
+
+        if(freeSpaces == 3 || rounds_elapsed >= 20 || (!start && ((position.amtR > 0 && position.amtB == 0) || (position.amtR == 0 && position.amtB > 0))))
+            game_end = 1;
+
+    }
+
+    gameOver(position,game_end);
+
 
     return 0;
 }
